97ab85f0f9837d83e0b351e62ee9fb38e6c98df0
Samuel Adjei <info.samadjei@gmail.com>
2021-05-24 22:24:03 +0100

    completed desktop hero styling

---
+++ components/hero.js
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '../components/button';
+
+const Hero = () => {
+	return (
+		<section className="hero">
+			<div className="container">
+				<div className="hero__flex">
+					<h1>Scooter sharing made simple</h1>
+					<div className="hero__inner">
+						<p className="hero__description">Scoot takes the hassle out of urban mobility. Our bikes are placed in convenient locations in each of our cities. Use our app to locate the nearest bike, unlock it with a tap, and you’re away!</p>
+						<Link className="hero__btn" href="/">
+							<Button className="btn hero--btn" buttonStyle="btn--primary" buttonSize="btn--desktop">
+								Get Scootin
+							</Button>
+						</Link>
+					</div>
+				</div>
+			</div>
+			<div className="hero__circle"></div>
+		</section>
+	);
+};
+
+export default Hero;
--- pages/homepage.js
+++ pages/homepage.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import Hero from '../components/hero';
 
 const Homepage = () => {
 	return (
 		<main>
-			<h1 className="title">
-				Welcome to <a href="https://nextjs.org">Next.js!</a>
-			</h1>
+			<Hero />
 		</main>
 	);
 };
