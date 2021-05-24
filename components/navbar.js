c84487fa5ecac1f91e63fffd9afbbc9d445fd5f5
Samuel Adjei <info.samadjei@gmail.com>
2021-05-24 17:20:06 +0100

    styled the button and styling the navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '../components/button';
 
 const Navbar = () => {
 	return (
-		<div>
-			<p>This is a navbar</p>
-		</div>
+		<nav className="logo container">
+			<Image src="/logo.svg" alt="Scoot logo" width={107.82} height={28.56} alt="Logo" />
+			<div className="nav-items">
+				<Link href="/about">
+					<a>About Us</a>
+				</Link>
+				<Link href="/about">
+					<a>Location</a>
+				</Link>
+				<Link href="/about">
+					<a>Careers</a>
+				</Link>
+			</div>
+			<div className="cta">
+				<Link href="/">
+					<Button className="btn hero--btn" buttonStyle="btn--primary" buttonSize="btn--desktop">
+						Get Scootin
+					</Button>
+				</Link>
+			</div>
+		</nav>
 	);
 };
 
