b8fe068d0a71580c8373a50f4e5bb8806b55d511
Samuel Adjei <info.samadjei@gmail.com>
2021-05-25 18:21:53 +0100

    added footer section

--- components/footer.js
+++ components/footer.js
@@ -5,19 +5,29 @@ const Footer = () => {
 	return (
 		<footer className="footer">
 			<div className="footer__top">
-				<div className=" footer__top--inner container">
+				<div className="footer__top-overlay"> </div>
+				<div className=" footer__top-inner container">
 					<h2 className="footer--title">Sign up and Scoot off today</h2>
 					<div className="footer__apps">
-						<Image className="footer__apps__child" src="/assets/icons/app-store.svg" alt="Apps Store" width={182} height={56} />
+						<div className="footer__apps-child">
+							<Image src="/assets/icons/app-store.svg" alt="Apps Store" width={182} height={56} />
+						</div>
+
 						<Image src="/assets/icons/google-play.svg" alt="Google Play" width={182} height={56} />
 					</div>
 				</div>
+				<div className="footer__top-bg">
+					<Image src="/assets/patterns/semi-circles.svg" alt="Google Play" width={1158} height={158} />
+				</div>
 			</div>
-			{/* <div className="footer__bottom">
-				<div className="footer__bottom--left">
-					<Image src="/logo.svg" alt="Apps Store" width={107.82} height={28.56} />
-					<div>
-						<div className="navbar__links">
+			<div className="footer__bottom">
+				<div className="container footer__bottom-inner">
+					<div className="footer__bottom-left">
+						<div className="footer__bottom-logo">
+							<Image src="/logo-white.svg" alt="Apps Store" width={107.82} height={28.56} />
+						</div>
+
+						<div className="footer__bottom-links">
 							<Link href="/about">
 								<a>About Us</a>
 							</Link>
@@ -29,13 +39,13 @@ const Footer = () => {
 							</Link>
 						</div>
 					</div>
+					<div className="footer__bottom-right">
+						<Image src="/assets/icons/facebook.svg" alt="Apps Store" width={24} height={24} />
+						<Image className="twitter" src="/assets/icons/twitter.svg" alt="Apps Store" width={24} height={24} />
+						<Image src="/assets/icons/instagram.svg" alt="Apps Store" width={24} height={24} />
+					</div>
 				</div>
-				<div className="footer__bottom--right">
-					<Image src="/assets/icons/facebook.svg" alt="Apps Store" width={107.82} height={28.56} />
-					<Image src="/assets/icons/twitter.svg" alt="Apps Store" width={107.82} height={28.56} />
-					<Image src="/assets/icons/instagram.svg" alt="Apps Store" width={107.82} height={28.56} />
-				</div>
-			</div> */}
+			</div>
 		</footer>
 	);
 };
