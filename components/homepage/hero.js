fe2868b3a3978c0a5a904273e4e1ed30b6fefd35
Samuel Adjei <info.samadjei@gmail.com>
2021-08-14 22:27:10 +0100

    added about faq section

--- components/homepage/hero.js
+++ components/homepage/hero.js
@@ -5,10 +5,9 @@ import Image from 'next/image';
 const Hero = () => {
 	return (
 		<section className="hero">
-			{/* <Image className="image" src="/public/assets/patterns/line.svg" width={202.5} height={15} /> */}
 			<div className="container">
 				<div className="hero__flex">
-					<h1>Scooter sharing made simple</h1>
+					<h1 className="hero__flex--title">Scooter sharing made simple</h1>
 					<div className="hero__inner">
 						<p className="hero__description">Scoot takes the hassle out of urban mobility. Our bikes are placed in convenient locations in each of our cities. Use our app to locate the nearest bike, unlock it with a tap, and you’re away!</p>
 						<Link className="hero__btn" href="/">
